ad94f2912ae18cfcdad13441a370a818011b4d23
Андрей Цыбульченко <tsybulchenko@yandex.ru>
2022-08-05 15:59:20 +0300

    1

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось,с 20 раза!')
 
 
 def second_page(request):
